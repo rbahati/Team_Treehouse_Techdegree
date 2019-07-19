deef0b539c079d9c515d84a2a9da1d5e282af10b
Raiss Bahati <raibahati@gmail.com>
2019-07-19 14:41:20 +0200

    Second Commit for the Techdegree second project. Added file(s): constants.py

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
+import constants
+
+
 if __name__ == "__main__":
     pass
+
+#  Define my app.py namespace constants variables
+
+PLAYERS = []
+TEAMS = []
+
+# Read from constants.py data (PLAYERS and TEAMS)
+
+# Build the PLAYERS data collection (a list of dictionaries)
+
+for player in constants.PLAYERS:
+    PLAYERS.append(player)
+
+# Build the TEAMS data collection (a list of string)
+
+for team in constants.TEAMS:
+    TEAMS.append(team)
+
+
+
+
